8aaa75ba21bacd9510156e7f4de9a74b117cc0be
arjungurjar01 <arjunsinghgurjar781@gmail.com>
2025-06-18 16:44:29 +0530

    chore(structure): add counter folder

---
+++ app/counter/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './components/Counter'
+
+function page() {
+  return (
+    <div>Naam jap counter page
+      <Counter/>
+    </div>
+  )
+}
+
+export default page
